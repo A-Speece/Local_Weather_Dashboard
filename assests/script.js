dcedc604d14251d0e8ff96f79509b513fcc36ff6
Anthony Speece <anthonyspeece@gmail.com>
2022-09-27 21:27:33 -0500

    added funcationality of search button in local storage

--- assests/script.js
+++ assests/script.js
@@ -1,7 +1,10 @@
 var searchCityFormEl = $("#search-form");
 var cityInputEl = $("#search-input");
-var currentCityEl = $("#currentcity");
+var currentCity = $("#current-city");
+
 var apiKey = "23ac2568a81ce4969e4c7f31ad40c4ed";
+var searchHistoryArray = [];
+var searchContainer = $(".search-container");
 
 function getCityDetail(cityName) {
   fetch(
@@ -41,13 +44,42 @@ function getCurrentWeather(lat, lon) {
     })
     .then(function (data) {
       console.log(data);
+      currentCity = currentCity.text(data.name);
     });
 }
 
 function citySearch(event) {
   event.preventDefault();
   var cityName = cityInputEl.val();
+  console.log(cityName);
+  setStorage(cityName);
   getCityDetail(cityName);
 }
+
+function setStorage(input) {
+  searchHistoryArray.push(input);
+  localStorage.setItem("citySearch", JSON.stringify(searchHistoryArray));
+  renderButtons();
+}
+
+function renderButtons() {
+  searchContainer.empty();
+  for (let I = 0; I < searchHistoryArray.length; I++) {
+    var button = document.createElement("button");
+    button.textContent = searchHistoryArray[I];
+    searchContainer.append(button);
+  }
+}
+
+function getCityStorage() {
+  var cityButton = localStorage.getItem("citySearch");
+
+  if (cityButton) {
+    searchHistoryArray = JSON.parse(cityButton);
+  }
+  renderButtons();
+}
+
+getCityStorage();
 
 searchCityFormEl.on("submit", citySearch);
